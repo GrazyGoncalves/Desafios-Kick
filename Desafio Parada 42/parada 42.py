b1fe56f4f0569d0558ec99652a7a77cbd4ee6e95
Graziella Goncalves <2007.graziella@gmail.com>
2023-08-01 21:49:36 -0300

    Desafio Parada 42
    exercicios python

---
+++ Desafio Parada 42/parada 42.py
@@ -0,0 +1,44 @@
+#exercicio 1 (digite 2 valores e calcule a soma)
+
+"""numero_1 = int(input(f"Digite um número: "))
+numero_2 = int(input(f"Digite um número: "))
+soma = numero_1 + numero_2
+
+print(f"A soma dos números {numero_1} e {numero_2} é: {soma}")
+
+#exercicio 2 (mostre os valores digitados na tela)
+
+nome = str(input(f"Digite seu nome: "))
+idade = int(input(f"Digite sua idade: "))
+telefone = int(input(f"Digite seu telefone: "))
+email = str(input(f"Digite seu e-mail: "))
+
+print(f"Mostrando os valores digitados: {nome}, {idade}, {telefone}, {email}")"""""
+
+#exercicio 3 (usuário cadastra informações, aparece na tela e faz a soma da quantidade e do preço)
+
+"""codigo = float(input(f"Digite o código: "))
+nome = str(input(f"Digite seu nome: "))
+quantidade = int(input(f"Digite a quantidade: "))
+preco = float(input(f"Digite o preço: R$ "))
+
+print(f"Mostrando as informações cadastradas: {codigo}, {nome}, {quantidade}, {preco}")
+
+valor_total = quantidade * preco
+
+print(f"O valor total da compra é R$ {valor_total:.2f}")"""
+
+#exercicio 4 (converter cm em metros)
+
+"""centimetros = int(input("Digite quantos cm quer converter para metros: "))
+metros = centimetros / 100
+print (metros)"""
+
+#exercicio 5 (calcula da área de um quadrado ou um retângulo)
+
+quadrado_A = int(input(f"Digite o valor A do quadrado: "))
+quadrado_B = int(input(f"Digite o valor B do quadrado: "))
+
+total_quadrado = quadrado_A * quadrado_B
+
+print(f"O valor total do quadrado é {total_quadrado} m2")
